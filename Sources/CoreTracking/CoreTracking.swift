3af5a912d40c3c84cf8d9d6d8f0e5841491fb9f5
Michael Schoder <m.schoder@my-realm.net>
2021-12-06 11:57:27 +0100

    Add CoreTracking base

--- Sources/CoreTracking/CoreTracking.swift
+++ Sources/CoreTracking/CoreTracking.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+public enum TrackingOutput {
+    case console
+    case logFiles
+}
+
+public class CoreTracking {
+    struct Configuration {
+        let outputs: Set<TrackingOutput>
+        let rootURL: URL
+
+        init(outputs: Set<TrackingOutput>, rootURL: URL? = nil) {
+            self.outputs = outputs
+            self.rootURL = rootURL ?? FileManager.default
+                .urls(for: .documentDirectory, in: .userDomainMask).first!
+                .appendingPathComponent("Insights")
+        }
+    }
+    private static var config: Configuration?
+
+    public static func configureInsights(_ outputs: Set<TrackingOutput>) {
+        if config != nil {
+            fatalError("CoreInsights was already configured")
+        }
+        Self.config = Configuration(outputs: outputs)
+    }
+}
--- Sources/Shared/Shared.swift
+++ Sources/Shared/Shared.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+public enum TrackingLevel: Int {
+    case `default` = 1000
+    case error = 2000
+    case warning = 3000
+    case info = 4000
+    case debug = 5000
+}
