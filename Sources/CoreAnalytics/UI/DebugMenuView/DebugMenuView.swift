1ebf203d193663e20b8e398240f359416b39b096
Michael Schoder <m.schoder@my-realm.net>
2021-12-06 19:16:11 +0100

    fix leftovers

--- Sources/CoreAnalytics/UI/DebugMenuView/DebugMenuView.swift
+++ Sources/CoreAnalytics/UI/DebugMenuView/DebugMenuView.swift
@@ -48,7 +48,7 @@ public struct DebugMenuView: View {
     }
 }
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 7.0, *)
+@available(iOS 14.0, watchOS 7.0, *)
 struct DebugMenuView_Previews: PreviewProvider {
     static var previews: some View {
         DebugMenuView(
